0659c97c6eb6a99bcea414293e3c3cf1f17255e9
Christian Baehr <cbaehr@aiddata.wm.edu>
2018-09-07 15:02:37 -0400

    documenting panel creation script

--- create_province_panel.R
+++ create_province_panel.R
@@ -1,20 +1,26 @@
 library(readxl)
 library(RCurl)
 
-
+#pull in AIMS data
 aims <- read.csv("~/Box Sync/cambodia_eba_gie/inputData/AIMS.csv")[,-1]
+#pull in Gazetteer data with province names and IDs
 province.data <- read.csv(text=getURL("https://raw.githubusercontent.com/itpir/cambodia_eba_gie/master/inputData/gazetteer_data_trimmed.csv"),
                     stringsAsFactors = F)
+#pull in Seila data
 seila <- read.csv("~/Box Sync/cambodia_eba_gie/inputData/Seila.csv", stringsAsFactors = F)
 
-#no AIMS data for the Tboung Khmum province
+#no AIMS data for the Tboung Khmum province. Ensuring AIMS province names correspond exactly to Gazetteer province 
+#names for merging purposes
 provinces <- names(aims)[4:28] <- c("Kampong Chhnang", "Kampong Thom", "Pursat", "Kampot", "Prey Veng", "Svay Rieng",
                        "Takeo", "Nation Wide", "Koh Kong", "Kampong Cham", "Kampong Speu", "Kratie", 
                        "Pailin", "Banteay Meanchey", "Phnom Penh", "Battambang", "Preah Sihanouk", 
                        "Preah Vihear", "Siem Reap", "Oddar Meanchey", "Kep", "Ratanak Kiri", 
                        "Stung Treng", "Kandal", "Mondul Kiri")
+#removing the "X" from the column names of the year dummies in the AIMS data
 years <- names(aims)[29:54] <- as.numeric(gsub("X", "", names(aims)[29:54]))
 
+#setting the year dummy equal to one for each year after the treatment takes effect (originally was only equal to one
+#on the specific year it took effect). This makes computing total years of funding easier 
 for(i in 1:nrow(aims)) {
   x <- which(names(aims)==as.character(years)[1])-1
   temp <- x+which(aims[i,as.character(years)]==1)
@@ -24,23 +30,38 @@ for(i in 1:nrow(aims)) {
   }
 }
 
+#for each project in the AIMS data, computing the product of the total number of provinces the project took place in
+#by the total number of years the project took place. This value is used to compute the average project budget allocated 
+#to each province in each year of the project.
+
+#we are splitting the years into 1993-2005 and 2006-2018 and placing 2x weight on the later set of years when computing 
+#average budget allocation
 aims$firsthalf_years_provs <- rowSums(aims[as.character(1993:2005)])*rowSums(aims[provinces])
 aims$secondhalf_years_provs <- rowSums(aims[as.character(2006:2018)])*rowSums(aims[provinces])
+#computing the average budget allocated to each project in each year for the years 1993-2005
 aims$avg_expend_firsthalf <- aims$Total.Budget/(aims$firsthalf_years_provs + (2*aims$secondhalf_years_provs))
+#computing the average budget allocated to each project in each year for the years 2006-2018. Each year/project unit
+#in the 2006-2018 subset is assigned twice the budget allocation of each year/project unit for 1993-2005
 aims$avg_expend_secondhalf <- aims$avg_expend_firsthalf*2
 
-###################
 
-province.data[25,] <- c(0, "Nation Wide")
+### Merging AIMS data with Gazetteer data ###
+
+#matching AIMS names with Gazetteer names
 province.data[17,2] <- "Siem Reap"
+#merging AIMS data with Gazetteer data based on province name
 province.data <- merge(as.data.frame(provinces, stringsAsFactors = F), province.data,
-                       by.x = "provinces", by.y = names(province.data)[2])
+                       by.x = "provinces", by.y = names(province.data)[2], all=T)
 
+#generating new columns for total number of projects taking place in each province in each year and also for total dollar
+#amount going into each province each year from these projects
 new.variables <- paste0(rep(c("count","sum"), each=length(years)), years)
+#combining the existing province data frame with these new columns
 province.data <- cbind(province.data, as.data.frame(matrix(NA, nrow = nrow(province.data), 
                                                         ncol = length(new.variables),
                                                         dimnames = list(c(),new.variables))))
 
+#filling the new columns with the total n projects and total amount of funding in each province each year
 for(i in 1:nrow(province.data)) {
   temp.data <- aims[which(aims[,which(colnames(aims)==province.data[i,1])]==1),]
 
@@ -49,13 +70,18 @@ for(i in 1:nrow(province.data)) {
                                       as.vector(t(as.matrix(temp.data[as.character(2006:2018)])) %*% as.matrix(temp.data["avg_expend_secondhalf"])))
 }
 
-###################
+### Merging province panel with Seila data ###
 
+#editing Seila column names for percent communes receiving treatment within each province
 names(seila)[10:17] <- gsub("X.", "seila_%communes_", names(seila)[10:17])
+#editing Seila column names for treatment dummies
 names(seila)[2:9] <- gsub("X", "seila_trt_", names(seila)[2:9])
+#editing Seila province names to align with province.data names for easier merging
 seila[1,"provinces"] <- "Siem Reap"
 
+#merging Seila data into the province panel data
 province.data <- merge(province.data, seila, by="provinces", all = T)
 
+#exporting the province level panel dataset containing both the AIMS and Seila data into Box Sync
 write.csv(province.data, "~/Box Sync/cambodia_eba_gie/processedData/eba_province_panel.csv")
 
