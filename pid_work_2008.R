ce8d01d3a09822b878fa45ee3b5e9c4c415326d3
Christian Baehr <cbaehr@aiddata.wm.edu>
2018-10-10 15:00:55 -0400

    updated 2008 pid code

--- pid_work_2008.R
+++ pid_work_2008.R
@@ -18,8 +18,8 @@ for(i in unique(progress$ContractID)) {
 }
 
 contract.output <- read_excel("~/Box Sync/cambodia_eba_gie/PID/pid_excel_2008/ContractOutput.xlsx")
-
-
+contract.output <- contract.output[!duplicated(contract.output$ContractID),c("ContractID", "linkProjectID")]
+contract <- merge(contract, contract.output, by="ContractID")
 
 ###################
 
@@ -54,4 +54,62 @@ project <- merge(project, proj.type, by.x="ProjTypeID", by.y="ProjTypeID")
 
 rm(list=setdiff(ls(), c("contract", "project", "project.output")))
 
-proj.test <- merge(project, project.output, by.x="ProjectID", by.y="ProjectID")
+project <- merge(project, project.output, by.x="ProjectID", by.y="ProjectID")
+
+which(duplicated(paste(project$VillGis, project$ProjectID)))[1]
+
+proj.cont <- cbind(project[0,], contract[0,])
+proj.cont[] <- lapply(proj.cont, function(x) as.numeric(as.character(x)))
+proj.cont <- proj.cont[,!(names(proj.cont)=="linkProjectID")]
+
+for(i in unique(contract$linkProjectID)) {
+  temp.contract <- contract[contract$linkProjectID==i,]
+  temp.project <- project[project$ProjectID==i,]
+  
+  if(nrow(temp.project)!=0) {
+    temp.project <- temp.project[!duplicated(temp.project$VillGis),]
+    
+    temp.contract[,c("FundCS", "FundLocalContr", "linkProjectID")] <- as.data.frame(matrix(c(temp.contract$FundCS/nrow(temp.project),
+                                                                                             temp.contract$FundLocalContr/nrow(temp.project),
+                                                                                             temp.contract$linkProjectID), ncol = 3))
+    
+    # cs.split <- as.data.frame(matrix(c(temp.contract$FundCS/nrow(temp.project),
+    #                                    temp.contract$FundLocalContr/nrow(temp.project),
+    #                                    temp.contract$linkProjectID), ncol = 3))
+    # names(cs.split) <- c("FundCS", "FundLocalContr", "linkProjectID")
+    
+    temp.project <- merge(temp.project, temp.contract, by.x="ProjectID", by.y="linkProjectID")
+    
+    # test <- merge(temp.project, temp.contract, by.x=)
+    # 
+    # 
+    # 
+    # temp.contract <- temp.contract[,!(names(temp.contract) %in% names(temp.project2))]
+    # temp.project3 <- merge(temp.project, temp.contract, by.x="ProjectID", by.y="linkProjectID")
+    # 
+    # temp.project4 <- cbind(temp.project2, temp.project3[,!(names(temp.project3) %in% names(temp.project))])
+    proj.cont[(nrow(proj.cont)+1):(nrow(proj.cont)+nrow(temp.project)),] <- temp.project
+  }
+}
+
+proj.cont <- proj.cont[,c("ProjectID", "ContractID", "NameE.y.x", "Name",
+                          "isNew", "NameE.y.y", )] #no subsector,
+names(proj.cont) <- c("project_id", "contract_id", "activity.type", "activity.desc",
+                      "new.repair.num", "new.repair",)
+
+write.csv(proj.cont, "~/Box Sync/")
+
+project <- project[,c("Id.x", "Id.y", "RILGPProjectTypeId", "Name_EN.y.y.2", "RILGPOutputCategoryId",
+                      "Name_EN.y.y", "SubSectorId.x", "List_Project_Output_Type_ID", "Name_EN.y.x",
+                      "plannedstartyear", "plannedstartmonth", "actualstartyear", "actualstartmonth",
+                      "plannedendyear", "plannedendmonth", "actualendyear", "actualendmonth", "last.report",
+                      "progress", "Bidders", "AwardedByBidding", "cs.fund", "local.cont", "VillageId.x")]
+#assigning meaningful names to the variables in the data
+names(project) <- c("project_id", "contract_id", "activity.type.num", "activity.type", "activity.desc.num",
+                    "activity.desc", "subsector", "new.repair.num", "new.repair", "planned.start.yr",
+                    "planned.start.mo", "actual.start.yr", "actual.start.mo", "planned.end.yr", "planned.end.mo",
+                    "actual.end.yr", "actual.end.mo", "last.report", "status", "n.bidders", "bid.dummy", "cs.fund",
+                    "local.cont", "vill.id")
+
+write.csv(project, "~/Box Sync/cambodia_eba_gie/PID/completed_pid/pid_2012.csv", row.names = F)
+
