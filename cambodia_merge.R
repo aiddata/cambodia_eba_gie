781a346fdbd77c512d02c00eb519a225210b1433
Christian Baehr <cbaehr@aiddata.wm.edu>
2018-11-05 16:24:42 -0500

    editing graphs|adding count panel

--- cambodia_merge.R
+++ cambodia_merge.R
@@ -7,6 +7,7 @@
 
 setwd("~/box sync/cambodia_eba_gie")
 
+library(dplyr)
 library(plyr)
 library(readxl)
 library(stringr)
@@ -15,14 +16,22 @@ library(stringr)
 # pid2012 <- read.csv("pid/completed_pid/pid_2012.csv", stringsAsFactors = F)
 # pid2008 <- read.csv("pid/completed_pid/pid_2008.csv", stringsAsFactors = F)
 # pid <- rbind.fill(pid2016, pid2012, pid2008)
-# common.index <- sort(unique(pid2008$activity.type)[which(((unique(pid2008$activity.type) %in% unique(pid2012$activity.type)) &
-#                                                (unique(pid2008$activity.type) %in% unique(pid2016$activity.type))))])
-# common.index <- cbind(common.index, seq(1, length(common.index), 1))
-# pid <- pid[,!(names(pid)=="activity.type.num")]
-# pid <- merge(pid, common.index, by.x="activity.type", by.y="common.index", all.x = T)
-# names(pid)[names(pid)=="V2"] <- "activity.type.num"
-# pid$activity.type.num <- as.numeric(pid$activity.type.num)
+# 
+# common.index <- as.data.frame(matrix(data = NA, nrow = 21, ncol = 0))
+# common.index[1:17,"2008"] <- sort(unique(pid2008$activity.type))
+# common.index[1:17,"2012"] <- sort(unique(pid2012$activity.type))[match(common.index[1:17,"2008"], 
+#                                                                        sort(unique(pid2012$activity.type)))]
+# common.index[1:17,"2016"] <- sort(unique(pid2016$activity.type))[match(common.index[1:17,"2008"], 
+#                                                                        sort(unique(pid2016$activity.type)))]
+# common.index[18:21, "2012"] <- unique(pid2012$activity.type)[!(unique(pid2012$activity.type) %in% common.index[1:17,"2012"])]
+# common.index[18:21, "2016"] <- unique(pid2016$activity.type)[match(common.index[18:21, "2012"], unique(pid2016$activity.type))]
+# common.index[,"type"] <- c(common.index[1:17,"2008"], common.index[18:21, "2012"])
+# common.index[,"id"] <- seq(1, 21, 1)
+# 
+# pid <- merge(pid[,!(names(pid)=="activity.type.num")], common.index[,c("type", "id")], by.x = "activity.type", by.y = "type")
+# names(pid)[names(pid)=="id"] <- "activity.type.num"
 # write.csv(pid, "pid/completed_pid/pid_merge.csv", row.names = F)
+
 pid <- read.csv("pid/completed_pid/pid_merge.csv",stringsAsFactors = F)
 pid <- pid[pid$actual.end.yr!=1908 | is.na(pid$actual.end.yr),]
 
@@ -133,31 +142,36 @@ data <- data[!is.na(data$actual.end.yr),]
 
 #building stacked barplots of activity type by year by province
 
-# for(i in unique(data$ProvEn)) {
-#   temp <- data[which((data$ProvEn==i & !is.na(data$activity.type) & !is.na(data$actual.end.yr))),]
-#   if(nrow(temp)>1) {
-#     temp.mat <- as.data.frame(matrix(0, ncol = 11, nrow = 16))
-#     a <- data[!duplicated(data[,c("activity.type", "activity.type.num")]), 
-#                     c("activity.type", "activity.type.num")]
-#     a <- a[!is.na(a$activity.type.num),]
-#     names(temp.mat) <- a$activity.type[match(c(1:11), a$activity.type.num)]
-#     row.names(temp.mat) <- c(2003:2018)
-#     
-#     for(j in unique(temp$actual.end.yr)) {
-#       temp2 <- temp[which(temp$actual.end.yr==j),]
-#       x <- table(temp2$activity.type.num)
-#       temp.mat[(row.names(temp.mat)==j), as.numeric(names(x))] <- table(temp2$activity.type.num)
-#     }
-#     png(paste0("/Users/christianbaehr/Box Sync/cambodia_eba_gie/descriptive_stats/activity_plots/activity_", 
-#                gsub(" ", "", i), ".png"), width = 10, height = 7, res = 300, units = 'in')
-#     barplot(t(as.matrix(temp.mat))[colSums(temp.mat)>0,], main = paste("Activity Type Distribution,", i),
-#             xlab = "Year", ylab = "Number of Projects", 
-#             col = which(colSums(temp.mat)>0)+5)
-#     legend("topright", legend=row.names(t(as.matrix(temp.mat)))[colSums(temp.mat)>0], cex = 0.75, 
-#            fill = which(colSums(temp.mat)>0)+5)
-#     dev.off()
-#   }
-# }
+for(i in unique(data$ProvEn)) {
+  temp <- data[which((data$ProvEn==i & !is.na(data$activity.type) & !is.na(data$actual.end.yr))),]
+  if(nrow(temp)>1) {
+    temp.mat <- as.data.frame(matrix(0, ncol = 21, nrow = 16))
+    a <- data[!duplicated(data[,c("activity.type", "activity.type.num")]),
+                    c("activity.type", "activity.type.num")]
+    a <- a[!is.na(a$activity.type.num),]
+    names(temp.mat) <- a$activity.type[match(c(1:21), a$activity.type.num)]
+    row.names(temp.mat) <- c(2003:2018)
+
+    for(j in unique(temp$actual.end.yr)) {
+      temp2 <- temp[which(temp$actual.end.yr==j),]
+      x <- table(temp2$activity.type.num)
+      temp.mat[(row.names(temp.mat)==j), as.numeric(names(x))] <- table(temp2$activity.type.num)
+    }
+    mycolors <- c('#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', 
+                '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', 
+                '#000000')
+    png(paste0("/Users/christianbaehr/Box Sync/cambodia_eba_gie/descriptive_stats/activity_plots/activity_",
+               gsub(" ", "", i), ".png"), width = 10, height = 7, res = 300, units = 'in')
+    barplot(t(as.matrix(temp.mat))[colSums(temp.mat)>0,], 
+            main = paste("Activity Type Distribution,", i), xlab = "Year", ylab = "Number of Projects",
+            col = mycolors[which(colSums(temp.mat)>0)])
+    legend("topright", legend=row.names(t(as.matrix(temp.mat)))[colSums(temp.mat)>0], cex = 0.75,
+           fill = mycolors[which(colSums(temp.mat)>0)])
+    dev.off()
+  }
+}
+
+########################
 
 gazetteer <- read_excel("/Users/christianbaehr/Box Sync/cambodia_eba_gie/inputData/National Gazetteer 2014.xlsx", sheet=3)
 data$commid <- NA
@@ -189,28 +203,28 @@ names(data)[names(data)=="Name_EN" ] <- "comm_name"
 # }
 # write.csv(graph.data, "/Users/christianbaehr/Box Sync/cambodia_eba_gie/descriptive_stats/treatment_rates/treatment_rates.csv",
 #           row.names = F)
-# graph.data <- read.csv("/Users/christianbaehr/Box Sync/cambodia_eba_gie/descriptive_stats/treatment_rates/treatment_rates.csv",
-#                        stringsAsFactors = F)
-# for(i in 1:nrow(graph.data)) {
-#   if(i==1) {
-#     png("/Users/christianbaehr/Box Sync/cambodia_eba_gie/descriptive_stats/treatment_rates/treatment_rate_graph.png",
-#         width = 10, height = 7, res = 300, units = 'in')
-#     plot(as.numeric(graph.data[1,]), col = i, type = "b", axes = F, xlab = NA, ylab = NA)
-#   } else {
-#     points(as.numeric(graph.data[i,]), col = i, type = "b")
-#   }
-#   if(i==nrow(graph.data)) {
-#     axis(side = 1, at = c(1:ncol(graph.data)), labels = gsub("X", "", names(graph.data)), tick = T)
-#     axis(side = 2, at = c(1:nrow(graph.data)/10), labels = paste0(seq(10, 100, 10), "%"),
-#          tick = T)
-#     mtext(side = 1, "Year", line = 2)
-#     mtext(side = 2, "% communes with > X% treatment", line = 2)
-#     mtext(side = 3, "% treatment by year at commune level")
-#     legend("bottomright", legend = paste0(seq(10, 100, 10), "% thres"), fill = c(1:nrow(graph.data)),
-#            col = c(1:nrow(graph.data)), cex = 1)
-#     dev.off()
-#   }
-# }
+graph.data <- read.csv("/Users/christianbaehr/Box Sync/cambodia_eba_gie/descriptive_stats/treatment_rates/treatment_rates.csv",
+                       stringsAsFactors = F)
+for(i in 1:nrow(graph.data)) {
+  if(i==1) {
+    png("/Users/christianbaehr/Box Sync/cambodia_eba_gie/descriptive_stats/treatment_rates/treatment_rate_graph.png",
+        width = 10, height = 7, res = 300, units = 'in')
+    plot(as.numeric(graph.data[1,]), col = i, type = "b", axes = F, xlab = NA, ylab = NA)
+  } else {
+    points(as.numeric(graph.data[i,]), col = i, type = "b")
+  }
+  if(i==nrow(graph.data)) {
+    axis(side = 1, at = c(1:ncol(graph.data)), labels = gsub("X", "", names(graph.data)), tick = T)
+    axis(side = 2, at = c(1:nrow(graph.data)/10), labels = paste0(seq(10, 100, 10), "%"),
+         tick = T)
+    mtext(side = 1, "Year", line = 2)
+    mtext(side = 2, "% communes with > X% treatment", line = 2)
+    mtext(side = 3, "% treatment by year at commune level")
+    legend("bottomright", legend = paste0(seq(10, 100, 10), "% thres"), fill = c(1:nrow(graph.data)),
+           col = c(1:nrow(graph.data)), cex = 1)
+    dev.off()
+  }
+}
 
 ###################
 
@@ -357,19 +371,39 @@ names(pre.panel) <-
   gsub("v4composites_calibrated_201709.", "ntl_", names(pre.panel)) %>%
   gsub(".mean", "", .)
 pre.panel <- pre.panel[!is.na(pre.panel$VillGis),]
-panel <- reshape(data = pre.panel, direction = "long", varying = grep("ntl_", names(pre.panel)),
+pre.panel[paste0("standardized_ntl_", 1992:2013)] <- apply(pre.panel[,grep("ntl_", names(pre.panel))], 2, 
+                                                           function(x) {x/max(x)})
+
+panel <- reshape(data = pre.panel, direction = "long", varying = list(paste0("ntl_", 1992:2013),
+                                                                      paste0("standardized_ntl_", 1992:2013)),
                  idvar = "panel_id", sep = "_", timevar = "year")
+names(panel)[names(panel)=="ntl_1992"] <- "ntl"
+names(panel)[names(panel)=="standardized_ntl_1992"] <- "standardized_ntl"
+
+
 # write.csv(panel, file = "/Users/christianbaehr/Box Sync/cambodia_eba_gie/ProcessedData/panel.csv", row.names = F)
+panel <- read.csv("/Users/christianbaehr/Box Sync/cambodia_eba_gie/ProcessedData/panel.csv", stringsAsFactors = F)
 
-#standardizing ntl values
-pre.panel2 <- pre.panel
-pre.panel2[,grep("ntl_", names(pre.panel2))] <- apply(pre.panel2[,grep("ntl_", names(pre.panel2))], 2, function(x) {x/max(x)})
-panel2 <- reshape(data = pre.panel2, direction = "long", varying = grep("ntl_", names(pre.panel2)),
-                 idvar = "panel_id", sep = "_", timevar = "year")
+###################
+
+panel2 <- reshape(data = pre.panel, direction = "long", varying = list(paste0("ntl_", 2003:2013),
+                                                                       paste0("standardized_ntl_", 2003:2013),
+                                                                       paste0("box.count", 2003:2013),
+                                                                       paste0("point.count", 2003:2013),
+                                                                       paste0("ltdr_avhrr_yearly_ndvi.", 2003:2013),
+                                                                       paste0("udel_air_temp_v4_01_yearly.", 2003:2013),
+                                                                       paste0("udel_precip_v4_01_yearly.", 2003:2013)),
+                  idvar = "panel_id", timevar = "year")
+
+names(panel2)[names(panel2)=="ntl_2003"] <- "ntl" 
+names(panel2)[names(panel2)=="standardized_ntl_2003"] <- "standardized_ntl"
+names(panel2)[names(panel2)=="box.count2003"] <- "box_count"
+names(panel2)[names(panel2)=="point.count2003"] <- "point_count"
+names(panel2)[names(panel2)=="ltdr_avhrr_yearly_ndvi.2003"] <- "ndvi"
+names(panel2)[names(panel2)=="udel_air_temp_v4_01_yearly.2003"] <- "temp"
+names(panel2)[names(panel2)=="udel_precip_v4_01_yearly.2003"] <- "precip"
+
 # write.csv(panel2, file = "/Users/christianbaehr/Box Sync/cambodia_eba_gie/ProcessedData/panel2.csv", row.names = F)
-
-plot(colMeans(pre.panel[,grep("ntl_", names(pre.panel))]), ylim = c(0,1))
-plot(colMeans(pre.panel2[,grep("ntl_", names(pre.panel2))]), ylim = c(0,1))
 
 
 
